41f7fb65b6742fd7ed43ad97fb696547c6fc91e9
Oleg Kravec <olegkravec97@gmail.com>
2020-07-03 17:16:29 +0300

    #20356 admin chat CHANGED mysql query to orm query

--- app/Models/RoomsMembersModel.php
+++ app/Models/RoomsMembersModel.php
@@ -13,20 +13,30 @@ class RoomsMembersModel extends Model
     public function getMember(){
         return $this->hasOne('App\User', 'id', 'user_id')->first();
     }
+
     public static function getMyRooms(){
         return self::hasMany('App\Models\RoomsModel', 'room_id', 'id')->get();
     }
 
     /**
-     * There is two functions that checks if the room already was created for selected user pair and returns room_id
+     * Method that check if two users already joined in the same room
+     *
      * @param $first_user
      * @param $second_user
+     * @param bool $alsoInGroup
      * @return mixed
      */
-    public static function isGroupConverstationExistWithUsersPair($first_user, $second_user){
-        return \DB::select(\DB::raw("SELECT RM.room_id FROM room_members as RM LEFT JOIN rooms r on RM.room_id = r.id WHERE r.isGroup = false AND RM.user_id = $first_user AND Exists (select * from room_members AS SRM where SRM.room_id = RM.room_id AND SRM.user_id = $second_user)"));
-    }
-    public static function isConverstationExistWithUsersPair($first_user, $second_user){
-        return \DB::select(\DB::raw("SELECT RM.room_id FROM room_members as RM LEFT JOIN rooms r on RM.room_id = r.id WHERE r.isGroup = false AND RM.user_id = $first_user AND Exists (select * from room_members AS SRM where SRM.room_id = RM.room_id AND SRM.user_id = $second_user)"));
+    public static function isConverstationExistWithUsersPair($first_user, $second_user, $alsoInGroup = false){
+        $room = RoomsModel::from("room_members as RM")->select("RM.room_id")->leftJoin('rooms as R', 'RM.room_id', '=', 'R.id')
+            ->where("R.isGroup", $alsoInGroup)
+            ->where("RM.user_id", $first_user)
+            ->whereExists(function ($query) use ($second_user) {
+                $query->select(\DB::raw("*"))
+                    ->from('room_members AS SECOND_RM')
+                    ->whereRaw('SECOND_RM.room_id = RM.room_id')
+                    ->where('SECOND_RM.user_id', $second_user);
+            })->get();
+
+        return $room;
     }
 }
