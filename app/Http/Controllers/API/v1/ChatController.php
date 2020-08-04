03bd696e364ab970bb7b44088aecc86d8b3109b5
Oleg <o.kravets@evnedev.com>
2020-08-04 16:57:22 +0300

    #20667 front on vue.js IMPLEMENTED page of chat

---
+++ app/Http/Controllers/API/v1/ChatController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Events\MessageFiredEvent;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\FireMessageRequest;
+use App\Models\MessagesModel;
+use App\Models\RoomsModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    /**
+     * Actually thats mathod that registers chat message event
+     * @param FireMessageRequest $request
+     * @param $room_id
+     */
+    public function fireMessage(FireMessageRequest $request, $room_id){
+        $room = RoomsModel::find($room_id);
+
+        // Send web-socket broadcast to selected room
+        broadcast(new MessageFiredEvent($room, Auth::user(), $request->validated()));
+
+        {
+            // Store message in DB
+            $message = new MessagesModel();
+            $message->room_id = $room_id;
+            $message->user_id = Auth::id();
+            $message->message = $request->validated()['message'];
+            $message->save();
+        }
+    }
+}
---
+++ stage2/src/components/Chats/ChatSelected/ChatSelected.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="ChatSelected">
+        <pre v-text="$attrs"/>
+        <div>
+            {{ $route.params.chatId }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+        },
+    };
+</script>
